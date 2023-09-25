6833096362aa85e1a886ba47c9a7511a1a60d611
muhammadrasif92 <rasif.aja@gmail.com>
2023-09-25 15:02:44 +0700

    first commit

---
+++ Kalkulator.py
@@ -0,0 +1,15 @@
+# Input dari pengguna
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+
+# Operasi kalkulator
+hasil_penjumlahan = angka1 + angka2
+hasil_pengurangan = angka1 - angka2
+hasil_perkalian = angka1 * angka2
+hasil_pembagian = angka1 / angka2
+
+# Menampilkan hasil
+print("Hasil penjumlahan:", hasil_penjumlahan)
+print("Hasil pengurangan:", hasil_pengurangan)
+print("Hasil perkalian:", hasil_perkalian)
+print("Hasil pembagian:", hasil_pembagian)
